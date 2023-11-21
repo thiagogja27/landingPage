4426075b327741a879f7672be9bd4e9a40e6fb8a
Thiago Nascimento <thiago_gja27@hotmail.com>
2023-11-21 16:52:38 -0300

    comimmit inicial

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputstyle:'compressed'}))
+    .pipe(sass({ outputStyle:'compressed'}))
     .pipe(gulp.dest('./dist/css'));
+}
+
+function images(){
+    return gulp.src('./src/imagens/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/imagens'));
 }
 
 
 
-exports.default = styles;
+exports.default = gulp.parallel(styles,images);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles))
